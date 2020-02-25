8dbab25a0e51bf661f810435a8c2208d6c582f32
Cian Capacci <cian.capacci@gmail.com>
2020-02-24 20:48:08 -0700

    Deleted the older filesystem

--- Zappin' Kraken v0.8.4/src/main/cpp/Subsystems/Feedback.cpp
+++
@@ -1,9 +0,0 @@
-#include "Subsystems/Feedback.h"
-
-Feedback::Feedback() : Subsystem("ExampleSubsystem") {}
-
-void Feedback::InitDefaultCommand() {}
-
-double Feedback::GetPressure() {
-  return (50 * m_pressure.GetVoltage()) - 25; 
-}
